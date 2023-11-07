e1a851f8577c960df7ffc36b1c3027f7b7b8a78b
masahiro <137256182+2201017@users.noreply.github.com>
2023-11-07 14:43:07 +0900

    変更

--- osusume3.php
+++ osusume3.php
@@ -8,9 +8,9 @@
 <?php require 'header.php' ?>
     <p>おすすめ</p>
     
-    <span onclick="alcohol()" value="お酒">お酒</span>
-    <span onclick="appetizers()" value="おつまみ">おつまみ</span>
-    <span onclick="alcoholset()" value="晩酌セット">晩酌セット<br></span>
+    <span onclick="alcohol()" value="ビール">お酒</span>
+    <span onclick="appetizers()" value="ワイン">おつまみ</span>
+    <span onclick="alcoholset()" value="リキュール">晩酌セット<br></span>
     <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;>
     <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;>
     <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;><br>
