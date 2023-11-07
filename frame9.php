8425568d00a82c20b1ff8bb7d27808172f7fafe3
masahiro <137256182+2201017@users.noreply.github.com>
2023-11-07 14:14:02 +0900

    追加

---
+++ frame9.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html1 long="ja">
+    <head>
+    <p>商品説明</p>
+    <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;>
+    <p>00000円  送料無料!</p>
+    <button type="submit">カートに入れる</button>
+    
