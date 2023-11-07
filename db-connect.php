db9898bc638948a3c148086adeb5504a2633d152
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-07 09:57:05 +0900

    change source

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql216.phy.lolipop.lan';
+    const DBNAME = 'LAA1516892-kaihatu';
+    const USER = 'LAA1516892';
+    const PASS = 'iu8yt5tghji9jhg';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
--- frame5.php
+++ frame5.php
@@ -1,11 +1,4 @@
-<?php
-    const SERVER = 'mysql216.phy.lolipop.lan';
-    const DBNAME = 'LAA1516892-kaihatu';
-    const USER = 'LAA1516892';
-    const PASS = 'iu8yt5tghji9jhg';
-
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-?>
+<?php require 'db-connect.php'; ?>
 <!DOCTYPE html>
     <html lang="ja">
         <head>
