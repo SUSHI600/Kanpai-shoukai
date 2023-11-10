e816eb9627406e194e9f5781df6147fc05fb9ee6
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-10 12:31:53 +0900

    change header

--- header.php
+++ header.php
@@ -1,27 +1,28 @@
 <link rel="stylesheet" href="css/header.css">
 <script src="js/jquery-3.7.0.min.js"></script>
+<script src="https://kit.fontawesome.com/8d94a879e2.js"></script>
 <script type="text/javascript" src="js/header.js"></script>
 <body>
   <header>
     <form action="" method="post">
       <span class="mod_dropnavi ex_clearfix">
         <ul class="topmenu">
-          <div>
+          <div class="logo">
             <img src="img/icon.png" alt="アイコン" width="180" height="50">
           </div>
           <div class="sbox">
-            <input type="text" class="searchbox" value="">
+            <input type="text" class="searchbox" placeholder="検索ワードを入力">
           </div>
           <div class="search">
-            <input type="image" src="img/search.png" alt="" width="30" height="30">
+            <button type="submit" class="searchbutton"><i class="fa-solid fa-magnifying-glass fa-lg"></i></button>
           </div>
           <div class="mypage">
-            <img src="img/mypage.png" alt="" width="50" height="50">
+            <i class="fa-solid fa-user size"></i>
           </div>
           <div class="cart">
-            <img src="img/cart.png" alt="" width="42" height="42">
+          <i class="fa-solid fa-cart-shopping size"></i>
           </div>
-          <li class="slidebtn"><a href="#"><img src="img/menu1.png" width="42" height="42" alt="メニュー" /></a>
+          <li class="slidebtn"><i class="fa-solid fa-bars size"></i></a>
             <ul class="submenu">
               <li><a href=""><img src="img/TOP.png" width="100" height="50"></a></li>
               <li><a href="mypage.php"><img src="img/login.png" width="100" height="50"></a></li>
