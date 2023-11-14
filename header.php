14c528279e0bd614a3f02097041227025a5f9b42
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-14 12:43:41 +0900

    change

--- header.php
+++ header.php
@@ -1,37 +1,33 @@
 <link rel="stylesheet" href="css/header.css">
-<script src="js/jquery-3.7.0.min.js"></script>
-<script src="https://kit.fontawesome.com/8d94a879e2.js"></script>
-<script type="text/javascript" src="js/header.js"></script>
 <body>
-  <header>
-    <form action="frame8.php" method="post">
-      <span class="mod_dropnavi ex_clearfix">
-        <ul class="topmenu">
-          <div class="logo">
-            <img src="img/icon.png" alt="アイコン" width="180" height="50">
-          </div>
-          <div class="sbox">
-            <input type="text" class="searchbox" placeholder="検索ワードを入力">
-          </div>
-          <div class="search">
-            <button type="submit" class="searchbutton"><i class="fa-solid fa-magnifying-glass fa-lg"></i></button>
-          </div>
-          <div class="mypage">
-            <i class="fa-solid fa-user size"></i>
-          </div>
-          <div class="cart">
-          <i class="fa-solid fa-cart-shopping size"></i>
-          </div>
-          <li class="slidebtn"><i class="fa-solid fa-bars size"></i></a>
-            <ul class="submenu">
-              <li><a href=""><img src="img/TOP.png" width="100" height="50"></a></li>
-              <li><a href="mypage.php"><img src="img/login.png" width="100" height="50"></a></li>
-              <li><a href=""><img src="img/osusume.png" width="100" height="50"></a></li>
-              <li><a href=""><img src="img/rireki.png" width="100" height="50"></a></li>
-            </ul>
-          </li>
-        </ul>
-      </span>
-    </form>
-  </header>
-</body>
+<header>
+  <form action="frame8.php" method="post">
+    <span class="mod_dropnavi ex_clearfix">
+      <ul class="topmenu">
+        <div class="logo">
+          <img src="img/icon.png" alt="アイコン" width="180" height="50">
+        </div>
+        <div class="sbox">
+          <input type="text" class="searchbox" placeholder="検索ワードを入力">
+        </div>
+        <div class="search">
+          <button type="submit" class="searchbutton"><i class="fa-solid fa-magnifying-glass fa-lg"></i></button>
+        </div>
+        <div class="mypage">
+          <i class="fa-solid fa-user size"></i>
+        </div>
+        <div class="cart">
+        <i class="fa-solid fa-cart-shopping size"></i>
+        </div>
+        <li class="slidebtn"><i class="fa-solid fa-bars size"></i></a>
+          <ul class="submenu">
+            <li><a href=""><img src="img/TOP.png" width="100" height="50"></a></li>
+            <li><a href="mypage.php"><img src="img/login.png" width="100" height="50"></a></li>
+            <li><a href=""><img src="img/osusume.png" width="100" height="50"></a></li>
+            <li><a href=""><img src="img/rireki.png" width="100" height="50"></a></li>
+          </ul>
+        </li>
+      </ul>
+    </span>
+  </form>
+</header>
