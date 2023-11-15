4cd6ad8e31d91a54dc98797cc4d53cd8c3269608
SUSHI600 <shinmatsuo0719@gmail.com>
2023-11-15 14:32:22 +0900

    change footer

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
-<script src="js/jquery-3.7.0.min.js"></script>
-<script src="https://kit.fontawesome.com/8d94a879e2.js"></script>
-<script type="text/javascript" src="js/header.js"></script>
+<script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
 </body>
 </html>
