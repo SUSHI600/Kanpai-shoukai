d8bf80e5480cf592f187a811ec4207479fa1c15a
Ryotoku <2201024@s.asojuku.ac.jp>
2023-11-15 13:52:23 +0900

    16kamosirenai

---
+++ frame16.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html1 long="ja">
+    <head>
+    <meta charset="UTF-8">
+    <?php require 'frame16.php' ?>
+</head>
+<body>
+<?php require 'header.php' ?>
+<img src="img/osake.jpg" alt="beer"width=150; heigth=50;>
+    <p>カテゴリ</p>
+    <button type="submit">お酒</button>
+    <button type="submit">おつまみ</button>
+    <button type="submit">晩酌セット</button>
+    </body>
