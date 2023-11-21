7228692d68f74da20e6755cf7d31808baf737458
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-21 14:59:10 +0900

    change

--- frame16.php
+++ frame16.php
@@ -1,15 +1,6 @@
-<?php require 'header.php'; ?>
-<!DOCTYPE html>
-<html1 long="ja">
-    <head>
-    <meta charset="UTF-8">
-    <?php require 'frame16.php' ?>
-</head>
-<body>
 <?php require 'header.php' ?>
 <img src="img/osake.jpg" alt="beer"width=150; heigth=50;>
-    <p>カテゴリ</p>
-    <button type="submit">お酒</button>
-    <button type="submit">おつまみ</button>
-    <button type="submit">晩酌セット</button>
-    </body>
+<p>カテゴリ</p>
+<button type="submit">お酒</button>
+<button type="submit">おつまみ</button>
+<button type="submit">晩酌セット</button>
