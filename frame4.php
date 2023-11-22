80a7571db034db13b7e71901953eaab46c0075a7
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-22 14:00:06 +0900

    change

--- frame4.php
+++ frame4.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    <?php require 'header.php' ?>
-    <h4>現在カートに商品が入っていません。</h4><br>
-    <span type="submit">戻る</span>
-</body>
-</html>
+<link rel="stylesheet" href="css/frame4.css">
+<?php require 'header.php' ?>
+<h4>現在カートに商品が入っていません。</h4><br>
+<span type="submit">戻る</span>
