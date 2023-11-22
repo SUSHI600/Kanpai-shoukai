78a42aaf042970c06a65c70ee9c2a6e5ea67f46e
SUSHI600 <shinmatsuo0719@gmail.com>
2023-11-22 14:39:25 +0900

    change src

--- login-output.php
+++ login-output.php
@@ -1,10 +1,10 @@
 <?php session_start(); ?>
+<?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 
 <?php
 unset($_SESSION['user']);
-$pdo=new PDO('mysql;host=localhost;dbname=kaihatu;charset=utf8',
-'staff', 'password');
+$pdo=new PDO('mysql:host=mysql216.phy.lolipop.lan;dbname=LAA1516892-kaihatu;charset=utf8','LAA1516892','iu8yt5tghji9jhg');
 $sql=$pdo->prepare('select * from user where login=? and password=?');
 $sql->execute([$_REQUEST['login'], $_REQUEST['password']]);
 foreach ($sql as $row) {
