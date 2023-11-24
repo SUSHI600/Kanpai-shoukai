0d89489ba0a2c5ac7651fa6675e97ecf4855b09b
yamada1023 <ayuka.yamada.tosu@gmail.com>
2023-11-24 10:27:40 +0900

    Update frame7.php

--- frame7.php
+++ frame7.php
@@ -2,9 +2,6 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>乾杯商会</title>
     <link rel="stylesheet" href="./css/frame7.css">
 
     <?php include './header.php'; ?>
