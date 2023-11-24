db4bf28d8fbf06a9d44063ff2d1662c56607d24a
yamada1023 <ayuka.yamada.tosu@gmail.com>
2023-11-24 15:49:54 +0900

    Update frame22.js

--- js/frame22.js
+++ js/frame22.js
@@ -1,19 +1,22 @@
 var postError = document.getElementById("postError");
 var post = document.getElementById("post");
+
 post.addEventListener('input', function () {
     var inputValue = this.value.replace(/[^0-9]/g, '');
 
     if (inputValue.length > 3) {
         inputValue = inputValue.slice(0, 3);
-    } else if (inputValue.length < 4) {
+    } else if (inputValue.length < 3) {
         postError.innerText = "郵便番号が不正です";
     } else {
         postError.innerText = "";
     }
+
     this.value = inputValue;
 });
 
 var code = document.getElementById("code");
+
 code.addEventListener('input', function () {
     var inputValue = this.value.replace(/[^0-9]/g, '');
 
@@ -24,31 +27,35 @@ code.addEventListener('input', function () {
     } else {
         postError.innerText = "";
     }
+
     this.value = inputValue;
 });
 
 var pass = document.getElementById("password");
 var passError = document.getElementById("passError");
 var word = document.getElementById("word");
+
 pass.addEventListener('input', function () {
     if (this.value === word.value || this.value.length == 0) {
         passError.innerText = "";
     } else {
         passError.innerText = "パスワードが一致しません。";
     }
-})
+});
 
 var mail = document.getElementById("mail");
 var mailError = document.getElementById("mailError");
+
 mail.addEventListener('input', function () {
     var email = mail.value;
-    const regex = /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+    var regex = /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+
     if (regex.test(email) || this.value.length == 0) {
         mailError.innerText = "";
     } else {
         mailError.innerText = "無効なメールアドレスです。";
     }
-})
+});
 
 function send() {
     var input = document.getElementsByClassName("input");
