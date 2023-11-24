011301e274dcad484b03e8631bcca8e1f11d38b2
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-24 14:26:27 +0900

    change

--- mpupdate.php
+++ mpupdate.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<?php session_start(); ?>
 <?php include 'header.php'; ?>
 <p>会員情報</p>
 <form action="mypage.php" method="post">
     <?php
-        echo '<p>ユーザーネーム:<input type="text" name="username" value="',$_SESSION['user']['name'],'"></p>';
-        echo '<p>パスワード:<input type="password" name="password" value="',$_SESSION['user']['password'],'"></p>';
-        echo '<p>メールアドレス:<input type="text" name="mailaddress" value="',$_SESSION['user']['e_mail'],'"></p>';
+        echo '<p>ユーザーネーム:<input type="text" name="username" value=""></p>';
+        echo '<p>パスワード:<input type="password" name="password" value=""></p>';
+        echo '<p>メールアドレス:<input type="text" name="mailaddress" value=""></p>';
     ?>
     <button type="submit">更新</button> 
 </form>
