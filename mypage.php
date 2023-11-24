f7139e84e9ecab2ecdadc587d7500d1373d99ba6
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-24 11:50:01 +0900

    change

--- mypage.php
+++ mypage.php
@@ -2,18 +2,21 @@
 <html lang="ja">
 <head>
 <?php include 'header.php'; ?>
-<?php session_start(); ?>
 <?php include 'db-connect.php'; ?>
 <h2>マイページ</h2>
 <?php
-    $pdo=new PDO($connect,USER,PASS);
-    $sql=$pdo->query('select * from user where id=8');
-    echo '<h2>ユーザーネーム</h2>';
-    echo $sql['name'];
-    echo '<h2>メールアドレス</h2>';
-    echo $sql['e_mail'];
-    echo '<h2>生年月日</h2>';
-    echo $sql['birthday'];
+    if($_SESSION['customer']){
+        $pdo=new PDO($connect,USER,PASS);
+        $sql=$pdo->query('select * from user where id=8');
+        echo '<h2>ユーザーネーム</h2>';
+        echo $sql['name'];
+        echo '<h2>メールアドレス</h2>';
+        echo $sql['e_mail'];
+        echo '<h2>生年月日</h2>';
+        echo $sql['birthday'];
+    }else{
+        echo 'ログインしてください';
+    }
 ?>
 <form action="mpupdate.php">
     <input type="submit" value="更新">
