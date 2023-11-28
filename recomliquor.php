a2473df5983dd6a9bf8f64444f5df083f519eebf
SUSHI600 <shinmatsuo0719@gmail.com>
2023-11-28 11:45:49 +0900

    change src

--- recomliquor.php
+++ recomliquor.php
@@ -11,9 +11,14 @@
     <span onclick="alcohol()" value="ビール">ビール</span>
     <span onclick="appetizers()" value="ワイン">ワイン</span>
     <span onclick="alcoholset()" value="リキュール">リキュール<br></span>
-    <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;>
-    <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;>
-    <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;><br>
-    <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;>
-    <img src="img/egao-mendako.png" alt="egao-mendako.png"width=150; heigth=50;>
+    <img src="img/la1.jpg" alt="liquor"width=150; heigth=50;>
+    <img src="img/laf1.jpg" alt="liquor"width=150; heigth=50;>
+    <img src="img/laf2.jpg" alt="liquor"width=150; heigth=50;><br>
+    <img src="img/laf3.jpg" alt="liquor"width=150; heigth=50;>
+    <img src="img/lam1.jpg" alt="liquor"width=150; heigth=50;>
+    <img src="img/lam2.jpg" alt="liquor"width=150; heigth=50;>
+    <img src="img/lam3.jpg" alt="liquor"width=150; heigth=50;>
+    <img src="img/le1.jpg" alt="liquor"width=150; heigth=50;>
+    <img src="img/le2.jpg" alt="liquor"width=150; heigth=50;>
+    <img src="img/le3.jpg" alt="liquor"width=150; heigth=50;>
     </body>
