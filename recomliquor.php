937e784d7890b447b1d1abee67bf692f7a50e008
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-28 14:01:25 +0900

    change

--- recomliquor.php
+++ recomliquor.php
@@ -6,8 +6,8 @@
     <p>おすすめ</p>
     
     <button onclick="location.href='recombeer.php'" value="ビール">ビール</button>
-    <button onclick="location.href='recomwine.php'" value="ワイン" style="">ワイン</button>
-    <button onclick="location.href='recomliquor.php'" value="リキュール">リキュール<br></button>
+    <button onclick="location.href='recomwine.php'" value="ワイン">ワイン</button>
+    <button onclick="location.href='recomliquor.php'" value="リキュール" style="backgrownd:grey">リキュール<br></button>
     <?php include './db-connect.php' ?>
     <?php
         $pdo=new PDO($connect,USER,PASS);
