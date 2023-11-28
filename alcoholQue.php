ccbe75397d52f1edec02f5a6b1bf4546955f64ab
SUSHI600 <shinmatsuo0719@gmail.com>
2023-11-28 11:41:11 +0900

    insert src

--- alcoholQue.php
+++ alcoholQue.php
@@ -1,4 +1,8 @@
 <link rel="stylesheet" href="css/Question.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossorigin="anonymous"/>
 <script src="js/question.js"></script>
 <h1>アンケート</h1>
 <span onclick="alcoholskip()">スキップ</span>
