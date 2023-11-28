a9330c3ce434aaf225b3f0dc5fbc28b30bc5e34b
SUSHI600 <shinmatsuo0719@gmail.com>
2023-11-28 11:18:54 +0900

    change src

--- alcoholQue.php
+++ alcoholQue.php
@@ -4,27 +4,27 @@
 <span onclick="alcoholskip()">スキップ</span>
 <ul>
     <li>味</li>
-    <input type="radio" name="taste" id="sweet" class="radiobutton">
-    <label for="sweet">甘め</label>
-    <input type="radio" name="taste" id="spicy" class="radiobutton">
-    <label for="spicy">辛め</label>
-    <input type="radio" name="taste" id="bitter" class="radiobutton">
-    <label for="bitter">苦め</label>
+    <input type="radio" class="btn-check" name="taste" id="sweet" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="sweet">甘い</label>
+    <input type="radio" class="btn-check" name="taste" id="spicy" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="spicy">辛い</label>
+    <input type="radio" class="btn-check" name="taste" id="bitter" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="bitter">苦い</label>
     <li>種類</li>
-    <input type="radio" name="kinds" id="beer" class="radiobutton">
-    <label for="beer">ビール</label>
-    <input type="radio" name="kinds" id="wine" class="radiobutton">
-    <label for="wine">ワイン</label>
-    <input type="radio" name="kinds" id="liqueur" class="radiobutton">
-    <label for="liqueur">リキュール</label>
+    <input type="radio" class="btn-check" name="kinds" id="beer" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="beer">ビール</label>
+    <input type="radio" class="btn-check" name="kinds" id="wine" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="wine">ワイン</label>
+<input type="radio" class="btn-check" name="kinds" id="liqueur" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="liqueur">リキュール</label>
     <li>地域</li>
-    <input type="radio" name="region" id="Asia" class="radiobutton">
-    <label for="Asia">アジア</label>
-    <input type="radio" name="region" id="America" class="radiobutton">
-    <label for="America">アメリカ</label>
-    <input type="radio" name="region" id="Europe" class="radiobutton">
-    <label for="Europe">ヨーロッパ</label>
-    <input type="radio" name="region" id="Africa" class="radiobutton">
-    <label for="Africa">アフリカ</label>
+    <input type="radio" class="btn-check" name="region" id="Asia" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="Asia">アジア</label>
+    <input type="radio" class="btn-check" name="region" id="America" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="America">アメリカ</label>
+    <input type="radio" class="btn-check" name="region" id="Europe" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="Europe">ヨーロッパ</label>
+    <input type="radio" class="btn-check" name="region" id="Africa" autocomplete="off">
+<label class="btn btn-secondary snackQue-label" for="Africa">アフリカ</label>
 </ul>
 <span>次へ</span>
