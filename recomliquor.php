f35c3ae8cb58f881b2233a3ce534268848e80321
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-28 12:13:07 +0900

    change

--- recomliquor.php
+++ recomliquor.php
@@ -1,24 +1,19 @@
 <!DOCTYPE html>
-<html1 long="ja">
-    <head>
-    <meta charset="UTF-8">
-    <script src="recommend.js"></script>
-</head>
-<body>
-<?php require 'header.php' ?>
+<html lang="ja">
+<head>
+<?php include './header.php' ?>
     <p>おすすめ</p>
     
     <span onclick="alcohol()" value="ビール">ビール</span>
     <span onclick="appetizers()" value="ワイン">ワイン</span>
     <span onclick="alcoholset()" value="リキュール">リキュール<br></span>
-    <img src="img/la1.jpg" alt="liquor"width=150; heigth=50;>
-    <img src="img/laf1.jpg" alt="liquor"width=150; heigth=50;>
-    <img src="img/laf2.jpg" alt="liquor"width=150; heigth=50;><br>
-    <img src="img/laf3.jpg" alt="liquor"width=150; heigth=50;>
-    <img src="img/lam1.jpg" alt="liquor"width=150; heigth=50;>
-    <img src="img/lam2.jpg" alt="liquor"width=150; heigth=50;>
-    <img src="img/lam3.jpg" alt="liquor"width=150; heigth=50;>
-    <img src="img/le1.jpg" alt="liquor"width=150; heigth=50;>
-    <img src="img/le2.jpg" alt="liquor"width=150; heigth=50;>
-    <img src="img/le3.jpg" alt="liquor"width=150; heigth=50;>
-    </body>
+    <?php include './db-connect.php' ?>
+    <?php
+        $pdo=new PDO($connect,USER,PASS);
+        $sql=$pdo->query('select * from items where id in (select id from liquors where liquor_id = 3)');
+        foreach($sql as $row){
+            $id = $row['id'];
+            echo '<a href="items.php?id=',$id,'">',"<img class='liquorlist' src=",$row['image'],">",'</a>';
+        }
+    ?>
+    <script src="recommend.js"></script>
