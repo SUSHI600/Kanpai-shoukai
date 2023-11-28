cda6d35d978fa5a30bb3d73ec16ecf76ca7ef3cd
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-11-28 14:03:05 +0900

    change

--- recombeer.php
+++ recombeer.php
@@ -5,7 +5,7 @@
 <?php include './header.php' ?>
     <p>おすすめ</p>
     
-    <button onclick="location.href='recombeer.php'" value="ビール" style="backgrownd:grey">ビール</button>
+    <button onclick="location.href='recombeer.php'" value="ビール" style="backgrownd:white">ビール</button>
     <button onclick="location.href='recomwine.php'" value="ワイン" style="">ワイン</button>
     <button onclick="location.href='recomliquor.php'" value="リキュール">リキュール<br></button>
     <?php include './db-connect.php' ?>
