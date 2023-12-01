3f8edd94d07edf151d087258b5c3ea2652f23aac
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-12-01 11:22:28 +0900

    change

--- cart-insert.php
+++ cart-insert.php
@@ -11,10 +11,6 @@
         $userid = $_SESSION['user']['id'];
         $sql=$pdo->prepare('insert into carts(add_date,quantity,item_id,user_id) values(:add_date,:quantity,:item_id,:user_id)');
         $sql->execute(array(':add_date'=>$add_date,':quantity'=>$quantity,'item_id'=>$item_id,'user_id'=>$userid));
-        echo $sql;
-        echo $_POST['count'];
-        echo $_POST['id'];
-        echo $_SESSION['user']['id'];
         echo '<p>カートに商品を追加しました。</p>';
         echo '<hr>';
         require 'frame5_show.php';
