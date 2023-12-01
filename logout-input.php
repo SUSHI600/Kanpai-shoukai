9e08627a388546d9cfd01940f69dc1faee5cc7c8
masahiro <137256182+2201017@users.noreply.github.com>
2023-12-01 14:41:39 +0900

    変更

--- logout-input.php
+++ logout-input.php
@@ -1,9 +1,10 @@
 <?php session_start(); ?>
+<link rel="stylesheet" href="css/logout-input.css">
 <?php
     if(isset($_SESSION['user'])){
         require 'header.php';
         echo '<p>ログアウトしますか？</p>';
-        echo '<a href="logout-output.php">ログアウト</a>';
+        echo '<a class="logout" href="logout-output.php">ログアウト</a>';
         require 'footer.php';
     }else{
         header('Location: ' . './login-input.php', true, 301);
