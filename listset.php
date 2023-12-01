e79fc154c10ffe61fcf316c29efb4ceb13aa0b94
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-12-01 14:58:09 +0900

    create

---
+++ listset.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet" href="./css/search.css">
+    <?php require 'db-connect.php' ?>
+    <?php require 'header.php' ?>
+    <?php
+        $pdo=new PDO($connect,USER,PASS);
+        $sql=$pdo->query('select * from items where category_id = 3');
+        foreach($sql as $row){
+            $id = $row['id'];
+            echo '<a href="items.php?id=',$id,'">',"<img class='liquorlist' src=",$row['image'],">",'</a>';
+        }
+    ?>
+    <?php require 'footer.php' ?>
