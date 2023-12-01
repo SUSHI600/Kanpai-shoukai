a65e767c505cf381852212c96b6fb852432f1ee7
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-12-01 10:09:06 +0900

    change

--- cart-insert.php
+++ cart-insert.php
@@ -9,8 +9,8 @@
         $quantity = $_POST['count'];
         $item_id = $_POST['id'];
         $userid = $_SESSION['user']['id'];
-        $sql=$pdo->prepare('insert into carts(add_date,quantity,item_id,user_id) values(:add_date,:quantity,item_id,user_id)');
-        $pdo->execute(array(':add_date'=>$add_date,':quantity'=>[$quantity],'item_id'=>[$item_id],'user_id'=>[$userid]));
+        $sql=$pdo->prepare('insert into carts(add_date,quantity,item_id,user_id) values(:add_date,:quantity,:item_id,:user_id)');
+        $pdo->execute(array(':add_date'=>$add_date,':quantity'=>$quantity,'item_id'=>$item_id,'user_id'=>$userid));
         echo $sql;
         echo $_POST['count'];
         echo $_POST['id'];
