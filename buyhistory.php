9267a16e328198b238c59ba8d8e2bc7f7d148fc7
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-12-04 10:42:32 +0900

    change

--- buyhistory.php
+++ buyhistory.php
@@ -4,6 +4,7 @@
 
 <head>
     <link rel="stylesheet" href="css/history.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
     <?php require 'db-connect.php'; ?>
     <?php require 'header.php'; ?>
     <?php
