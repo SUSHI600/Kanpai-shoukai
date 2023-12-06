0d2fada5fb78c009386d805acb175ba6672e4b70
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-12-06 09:59:03 +0900

    change

--- logout-output.php
+++ logout-output.php
@@ -1,12 +1,17 @@
 <?php session_start () ; ?>
-<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
 
-<?php
-if (isset ($_SESSION['user'])) {
-unset ($_SESSION['user']);
-echo'ログアウトしました。';
-} else {
-echo'すでにログアウトしています。';
-}
-?>
-<?php require 'footer.php'; ?>
+<head>
+    <link rel="stylesheet" href="css/logout-input.css">
+    <?php require 'header.php'; ?>
+
+    <?php
+    if (isset ($_SESSION['user'])) {
+    unset ($_SESSION['user']);
+    echo'<p>ログアウトしました。</p>';
+    } else {
+    echo'<p>すでにログアウトしています。</p>';
+    }
+    ?>
+    <?php require 'footer.php'; ?>
