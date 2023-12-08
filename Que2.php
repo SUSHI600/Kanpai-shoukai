1ce296fffd58f97ba65d3208a9a79bd68ac68573
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2023-12-08 12:28:24 +0900

    change

--- Que2.php
+++ Que2.php
@@ -19,19 +19,24 @@
             echo '<!DOCTYPE html>';
             echo '<html lang="ja">';
             echo '<head>';
+            echo '<link rel="stylesheet" href="css/Que.css">';
             require 'header2.php';
             if(!isset($_SESSION['user']['id'])){
                 echo 'ログインしてください';
                 echo '<a href="login-input.php">ログイン/新規登録へ</a>';
             }elseif(!isset($_POST['taste']) || !isset($_POST['region'])){
-                echo '<p>選択していない項目があります</p>';
-                echo '<p>アンケートをやりなおしてください</p>';
+                echo '<h3>選択していない項目があります</h3>';
+                echo '<h3>アンケートをやりなおしてください</h3>';
                 echo '<a href="alcoholQue.php">アンケートに戻る</a>';
             }
         }
         // ... 以降のコード ...
     } catch (PDOException $e) {
-        echo '＊情報が重複しています＊';
+        echo '<!DOCTYPE html>';
+        echo '<html lang="ja">';
+        echo '<head>';
+        echo '<link rel="stylesheet" href="css/Que.css">';
+        echo '<h3>＊情報が重複しています＊</h3>';
         echo '<a href="alcoholQue.php">アンケートに戻る</a>';
     }
 ?>
